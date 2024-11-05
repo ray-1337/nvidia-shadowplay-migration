5ed9681e563c5f912487fe0eda6a8f9e199c6539
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2024-11-05 13:09:21 +0800

    add a configuration file

---
+++ config.mjs
@@ -0,0 +1,12 @@
+export const ROOT_DIR_PATH = "";
+
+export const DESTINATION_DIR_PATH = "";
+
+export const SOUVENIR = {
+  // i usually save projects folder such as from Adobe Premiere Pro directly inside the nested folder
+  projects: ".PROJECTS",
+
+  footages: ".RAW_FOOTAGE_NVIDIA",
+
+  screenshots: ".SCREENSHOTS"
+};
